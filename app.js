da9fde181c42a90fcd83e0f2c1498359a5fd138d
Julian Häring <Julian.Haering@plan-b-gmbh.com>
2020-03-28 17:02:17 +0100

    navbar

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const hbs = require('express-handlebars');
 
+
+
+//routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -13,14 +17,27 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultView: 'default',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials'
+}));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
+
+
+//routen
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
